1a0b99d69f36482948317da6c2cd1de12941312f
MohamedBenabdelhadi <simobenabdelhadi89@gmail.com>
2024-12-10 07:19:53 +0100

    3.2_I2C_Mem_Read_CHIP_ID

--- Projet_STM/Core/Src/main.c
+++ Projet_STM/Core/Src/main.c
@@ -18,221 +18,222 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
+#include "dma.h"
+#include "i2c.h"
+#include "sai.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "Driver_LED.h"
-#include "shell.h"
-#include "FreeRTOS.h"
-#include "task.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
+/* USER CODE END Includes */
 
-/* Private defines -----------------------------------------------------------*/
-#define TASK_SHELL_STACK_DEPTH 512
-#define TASK_SHELL_PRIORITY 1
-#define BUFFER_SIZE 64
-static char print_buffer[BUFFER_SIZE];
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define SGTL5000_I2C_ADDR  0x14  // Adresse I2C du SGTL5000
+#define CHIP_ID_REG        0x0000 // Adresse du registre CHIP_ID
+#define TEST_REGISTER      0x0030//CHIP_ANA_POWER
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
 /* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void PeriphCommonClock_Config(void);
 void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
 
-TaskHandle_t h_task_shell = NULL;
-Driver_LED_HandleTypeDef led_driver = {
-		.hspi = &hspi3,
-		.cs_port = GPIOB,
-		.cs_pin = GPIO_PIN_7
-};/* Redirect printf to UART */
+/* USER CODE END PFP */
 
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 int __io_putchar(int ch) {
 	HAL_UART_Transmit(&huart2, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
 	return ch;
 }
-static int uart_write(char *s, uint16_t size) {
-	HAL_UART_Transmit(&huart2, (uint8_t*)s, size, HAL_MAX_DELAY);
-	return size;
-}
-static int sh_led(int argc, char **argv) {
-	if (argc < 3) { // Vérifie si le nombre d'arguments est suffisant
-		int size = snprintf(print_buffer, BUFFER_SIZE, "Usage: l <led_number> <state>\r\n");
-		uart_write(print_buffer, size);
-		return -1;
-	}
 
-	int led_number = atoi(argv[1]); // Convertit le numéro de la LED en entier
-	int state = atoi(argv[2]);      // Convertit l'état en entier
-
-	if (led_number < 0 || led_number > 15) { // Vérifie si le numéro de LED est valide
-		int size = snprintf(print_buffer, BUFFER_SIZE, "Error: Invalid LED number. Must be between 0 and 15.\r\n");
-		uart_write(print_buffer, size);
-		return -1;
-	}
-
-	if (state != 0 && state != 1) { // Vérifie si l'état est valide
-		int size = snprintf(print_buffer, BUFFER_SIZE, "Error: Invalid state. Must be 0 or 1.\r\n");
-		uart_write(print_buffer, size);
-		return -1;
-	}
-
-	// Détermine si la LED est sur GPIOA ou GPIOB
-	if (led_number < 8) {
-		uint8_t mask = 1 << led_number;
-		uint8_t current_state = Driver_LED_ReadLEDA(&led_driver);
-
-		if (state == 1) {
-			Driver_LED_SetLEDA(&led_driver, current_state & ~mask); // Allume la LED
-		} else {
-			Driver_LED_SetLEDA(&led_driver, current_state | mask);  // Éteint la LED
-		}
-	} else {
-		uint8_t mask = 1 << (led_number - 8);
-		uint8_t current_state = Driver_LED_ReadLEDB(&led_driver);
-
-		if (state == 1) {
-			Driver_LED_SetLEDB(&led_driver, current_state & ~mask); // Allume la LED
-		} else {
-			Driver_LED_SetLEDB(&led_driver, current_state | mask);  // Éteint la LED
+void I2C_Scan(void) {
+	printf("Scanning I2C bus\r\n");
+	for (uint16_t addr = 0; addr < 128; addr++) {
+		if (HAL_I2C_IsDeviceReady(&hi2c2, addr , 2, HAL_MAX_DELAY) == HAL_OK) {
+			printf("Périphérique trouvé à l'adresse 0x%02X\r\n", addr);
 		}
 	}
-
-	int size = snprintf(print_buffer, BUFFER_SIZE, "LED %d set to %d\r\n", led_number, state);
-	uart_write(print_buffer, size);
-
-	return 0;
+	printf("Scan terminé.\r\n");
 }
 
-static int sh_led_all_on(int argc, char **argv) {
-	Driver_LED_SetLEDA(&led_driver, 0x00); // Allume toutes les LEDs sur PORTA
-	Driver_LED_SetLEDB(&led_driver, 0x00); // Allume toutes les LEDs sur PORTB
+void Read_CHIP_ID_Debug(void) {
+    uint8_t chip_id[2] = {0};
 
-	int size = snprintf(print_buffer, BUFFER_SIZE, "All LEDs are ON\r\n");
-	uart_write(print_buffer, size);
-	return 0;
+    printf("Lecture du registre CHIP_ID \r\n");
+
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(&hi2c2, SGTL5000_I2C_ADDR, CHIP_ID_REG, I2C_MEMADD_SIZE_16BIT, chip_id, 2, HAL_MAX_DELAY);
+    if (status == HAL_OK) {
+        // Appliquer le décalage de 8 bits pour le MSB
+        uint16_t chip_id_val = (chip_id[0] << 8) | chip_id[1];
+        printf("CHIP_ID lu avec succès : 0x%04X\r\n", chip_id_val);
+    } else {
+        printf("Erreur : Impossible de lire le CHIP_ID\r\n");
+        uint32_t i2c_error = HAL_I2C_GetError(&hi2c2);
+        printf("Erreur I2C\r\n", i2c_error);
+    }
 }
-
-void task_shell(void * unused)
-{
-	shell_init();
-	shell_add('l', sh_led, "Led <led_number> <state> - Control LED");
-	shell_add('a', sh_led_all_on, "Allumer toutes les LEDs");
-	shell_run();	// boucle infinie
-}
-/* USER CODE BEGIN 0 */
-
 
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-	/* MCU Configuration--------------------------------------------------------*/
-	HAL_Init();
-	SystemClock_Config();
-	MX_GPIO_Init();
-	MX_USART2_UART_Init();
-	MX_SPI3_Init();
 
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+  /* USER CODE BEGIN 1 */
 
-	// Initialiser le driver avant d'exécuter le shell
-	Driver_LED_Init(&led_driver);
+  /* USER CODE END 1 */
 
+  /* MCU Configuration--------------------------------------------------------*/
 
-	if (xTaskCreate(task_shell, "Shell", TASK_SHELL_STACK_DEPTH, NULL, TASK_SHELL_PRIORITY, &h_task_shell) != pdPASS) {
-		printf("Error creating task shell\r\n");
-		Error_Handler();
-	}
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	vTaskStartScheduler();
+  /* USER CODE BEGIN Init */
 
-	/* Infinite loop */
-	while (1) {
+  /* USER CODE END Init */
 
+  /* Configure the system clock */
+  SystemClock_Config();
 
-		//				Driver_LED_SetLEDB(&led_driver, 0x00); // Toutes les LEDs sur PORTA ON
-		//				HAL_Delay(100);
-		//
-		//				// Éteindre toutes les LEDs sur PORTA
-		//				Driver_LED_SetLEDB(&led_driver, 0xFF); // Toutes les LEDs sur PORTA OFF
-		//				HAL_Delay(100);
-		//
-		//				Driver_LED_SetLEDA(&led_driver, 0x00); // Toutes les LEDs sur PORTA ON
-		//				HAL_Delay(100);
-		//
-		//				// Éteindre toutes les LEDs sur PORTA
-		//				Driver_LED_SetLEDA(&led_driver, 0xFF); // Toutes les LEDs sur PORTA OFF
-		//				HAL_Delay(100);
-		//
-		//				// Boucle pour allumer les LEDs sur PORTA (LEDs 1 à 8)
-		//				for (uint8_t i = 0; i < 8; i++) {
-		//					Driver_LED_SetLEDA(&led_driver, ~(1 << i)); // Active bas : ~ pour inverser
-		//					HAL_Delay(500);
-		//					Driver_LED_SetLEDA(&led_driver, 0xFF); // Éteindre après chaque étape
-		//				}
-		//
-		//				// Boucle pour allumer les LEDs sur PORTB (LEDs 9 à 16)
-		//				for (uint8_t i = 0; i < 8; i++) {
-		//					Driver_LED_SetLEDB(&led_driver, ~(1 << i)); // Active bas : ~ pour inverser
-		//					HAL_Delay(500);
-		//					Driver_LED_SetLEDB(&led_driver, 0xFF); // Éteindre après chaque étape
-		//				}
+  /* Configure the peripherals common clocks */
+  PeriphCommonClock_Config();
 
-	}
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  MX_SPI3_Init();
+  MX_I2C2_Init();
+  MX_SAI2_Init();
+  /* USER CODE BEGIN 2 */
+  __HAL_SAI_ENABLE(&hsai_BlockA2);
+  printf("Demarrage du systeme...\r\n");
+  I2C_Scan();
+  	Read_CHIP_ID_Debug();
+  /* USER CODE END 2 */
+
+  /* Call init function for freertos objects (in cmsis_os2.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
 }
+
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = 1;
-	RCC_OscInitStruct.PLL.PLLN = 10;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Peripherals Common Clock Configuration
+  * @retval None
+  */
+void PeriphCommonClock_Config(void)
+{
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the peripherals clock
+  */
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
+  PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
+  PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
+  PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+  PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
+  PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
+  PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+  PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+  PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -240,33 +241,33 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- Projet_STM/Debug/Core/Src/subdir.mk
+++ Projet_STM/Debug/Core/Src/subdir.mk
@@ -6,9 +6,12 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Core/Src/Driver_LED.c \
+../Core/Src/dma.c \
 ../Core/Src/freertos.c \
 ../Core/Src/gpio.c \
+../Core/Src/i2c.c \
 ../Core/Src/main.c \
+../Core/Src/sai.c \
 ../Core/Src/shell.c \
 ../Core/Src/spi.c \
 ../Core/Src/stm32l4xx_hal_msp.c \
@@ -20,9 +23,12 @@ C_SRCS += \
 
 OBJS += \
 ./Core/Src/Driver_LED.o \
+./Core/Src/dma.o \
 ./Core/Src/freertos.o \
 ./Core/Src/gpio.o \
+./Core/Src/i2c.o \
 ./Core/Src/main.o \
+./Core/Src/sai.o \
 ./Core/Src/shell.o \
 ./Core/Src/spi.o \
 ./Core/Src/stm32l4xx_hal_msp.o \
@@ -34,9 +40,12 @@ OBJS += \
 
 C_DEPS += \
 ./Core/Src/Driver_LED.d \
+./Core/Src/dma.d \
 ./Core/Src/freertos.d \
 ./Core/Src/gpio.d \
+./Core/Src/i2c.d \
 ./Core/Src/main.d \
+./Core/Src/sai.d \
 ./Core/Src/shell.d \
 ./Core/Src/spi.d \
 ./Core/Src/stm32l4xx_hal_msp.d \
@@ -54,7 +63,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/Driver_LED.cyclo ./Core/Src/Driver_LED.d ./Core/Src/Driver_LED.o ./Core/Src/Driver_LED.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/shell.cyclo ./Core/Src/shell.d ./Core/Src/shell.o ./Core/Src/shell.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/stm32l4xx_hal_msp.cyclo ./Core/Src/stm32l4xx_hal_msp.d ./Core/Src/stm32l4xx_hal_msp.o ./Core/Src/stm32l4xx_hal_msp.su ./Core/Src/stm32l4xx_it.cyclo ./Core/Src/stm32l4xx_it.d ./Core/Src/stm32l4xx_it.o ./Core/Src/stm32l4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l4xx.cyclo ./Core/Src/system_stm32l4xx.d ./Core/Src/system_stm32l4xx.o ./Core/Src/system_stm32l4xx.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
+	-$(RM) ./Core/Src/Driver_LED.cyclo ./Core/Src/Driver_LED.d ./Core/Src/Driver_LED.o ./Core/Src/Driver_LED.su ./Core/Src/dma.cyclo ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/i2c.cyclo ./Core/Src/i2c.d ./Core/Src/i2c.o ./Core/Src/i2c.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/sai.cyclo ./Core/Src/sai.d ./Core/Src/sai.o ./Core/Src/sai.su ./Core/Src/shell.cyclo ./Core/Src/shell.d ./Core/Src/shell.o ./Core/Src/shell.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/stm32l4xx_hal_msp.cyclo ./Core/Src/stm32l4xx_hal_msp.d ./Core/Src/stm32l4xx_hal_msp.o ./Core/Src/stm32l4xx_hal_msp.su ./Core/Src/stm32l4xx_it.cyclo ./Core/Src/stm32l4xx_it.d ./Core/Src/stm32l4xx_it.o ./Core/Src/stm32l4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l4xx.cyclo ./Core/Src/system_stm32l4xx.d ./Core/Src/system_stm32l4xx.o ./Core/Src/system_stm32l4xx.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
 
 .PHONY: clean-Core-2f-Src
 
